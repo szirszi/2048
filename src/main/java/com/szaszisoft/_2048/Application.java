a229f9ef613d999806d360c8a8e3aa7bb3d33fba
Szabolcs Milan <szirszi@gmail.com>
2023-10-09 18:10:20 +0200

    Add game (two boards together)

--- src/main/java/com/szaszisoft/_2048/Application.java
+++ src/main/java/com/szaszisoft/_2048/Application.java
@@ -1,7 +1,13 @@
 package com.szaszisoft._2048;
 
 import com.szaszisoft._2048.models.Board;
+import com.szaszisoft._2048.models.BoardType;
+import com.szaszisoft._2048.models.Game;
+import com.szaszisoft._2048.repositories.GameRepository;
 import com.szaszisoft._2048.services.BoardService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +15,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
   private final BoardService boardService;
+  private final GameRepository gameRepository;
 
-  public Application(BoardService boardService) {
+  @Autowired
+  public Application(BoardService boardService, GameRepository gameRepository) {
     this.boardService = boardService;
+    this.gameRepository = gameRepository;
   }
 
   public static void main(String[] args) {
@@ -20,11 +29,19 @@ public class Application implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-
+    Game game = gameRepository.save(new Game());
     Board board = boardService.initializeBoard(4);
+    board.setGame(game);
+    board.setBoardType(BoardType.PLAY);
     System.out.println("Play: " + board.getId());
     Board previous = boardService.initializeBoard(4);
+    previous.setGame(game);
+    previous.setBoardType(BoardType.UNDO);
     System.out.println("Prev: " + previous.getId());
+    List<Board> boards = new ArrayList<>();
+    boards.add(board);
+    boards.add(previous);
+    game.setBoards(boards);
+    gameRepository.save(game);
   }
 }
-
