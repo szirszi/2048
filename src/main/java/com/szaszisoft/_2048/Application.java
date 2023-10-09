f2004bd3f1765712cbb74d8a14a3c2879fb6e290
Szabolcs Milan <szirszi@gmail.com>
2023-10-09 22:03:40 +0200

    Remove develop/test lines

--- src/main/java/com/szaszisoft/_2048/Application.java
+++ src/main/java/com/szaszisoft/_2048/Application.java
@@ -1,12 +1,7 @@
 package com.szaszisoft._2048;
 
-import com.szaszisoft._2048.models.Board;
-import com.szaszisoft._2048.models.BoardType;
-import com.szaszisoft._2048.models.Game;
-import com.szaszisoft._2048.repositories.GameRepository;
 import com.szaszisoft._2048.services.BoardService;
-import java.util.ArrayList;
-import java.util.List;
+import com.szaszisoft._2048.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,12 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application implements CommandLineRunner {
   private final BoardService boardService;
-  private final GameRepository gameRepository;
+  private final GameService gameService;
 
   @Autowired
-  public Application(BoardService boardService, GameRepository gameRepository) {
+  public Application(BoardService boardService, GameService gameService) {
     this.boardService = boardService;
-    this.gameRepository = gameRepository;
+    this.gameService = gameService;
   }
 
   public static void main(String[] args) {
@@ -29,19 +24,9 @@ public class Application implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    Game game = gameRepository.save(new Game());
-    Board board = boardService.initializeBoard(4);
-    board.setGame(game);
-    board.setBoardType(BoardType.PLAY);
-    System.out.println("Play: " + board.getId());
-    Board previous = boardService.initializeBoard(4);
-    previous.setGame(game);
-    previous.setBoardType(BoardType.UNDO);
-    System.out.println("Prev: " + previous.getId());
-    List<Board> boards = new ArrayList<>();
-    boards.add(board);
-    boards.add(previous);
-    game.setBoards(boards);
-    gameRepository.save(game);
+/*
+    gameService.startNewGame(8);
+    gameService.startNewGame(5);
+*/
   }
 }
