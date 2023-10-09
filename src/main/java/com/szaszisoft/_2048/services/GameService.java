076d800b781397e7433e9230f60dde3caad9547f
Szabolcs Milan <szirszi@gmail.com>
2023-10-09 22:07:31 +0200

    Multiboard set up

--- src/main/java/com/szaszisoft/_2048/services/GameService.java
+++ src/main/java/com/szaszisoft/_2048/services/GameService.java
@@ -1,7 +1,10 @@
 package com.szaszisoft._2048.services;
 
 import com.szaszisoft._2048.models.Game;
+import java.util.List;
 
 public interface GameService {
   Game getGameById(long id);
+  List<Game> findAll();
+  Game startNewGame(Integer size);
 }
