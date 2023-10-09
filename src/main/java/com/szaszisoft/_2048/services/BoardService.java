a8b16bdd6e7d08d030b9423b31ed1e3f8b8c1396
Szabolcs Milan <szirszi@gmail.com>
2023-10-09 16:52:27 +0200

    Add methods to get and put data from/to database, and start new game

--- src/main/java/com/szaszisoft/_2048/services/BoardService.java
+++ src/main/java/com/szaszisoft/_2048/services/BoardService.java
@@ -1,4 +1,11 @@
 package com.szaszisoft._2048.services;
 
+import com.szaszisoft._2048.models.Board;
+
 public interface BoardService {
+  Board initializeBoard(Integer size);
+  Board getBoardById(Long id);
+
+  Board save(Board board);
+  Board resetBoard(Board board);
 }
