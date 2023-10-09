b88d6815f58779bb4f5f12b26eb30e9d51594dff
Szabolcs Milan <szirszi@gmail.com>
2023-10-09 22:09:17 +0200

    Keep track the board through cookie

--- src/main/java/com/szaszisoft/_2048/controllers/WebController.java
+++ src/main/java/com/szaszisoft/_2048/controllers/WebController.java
@@ -1,15 +1,21 @@
 package com.szaszisoft._2048.controllers;
 
+import com.szaszisoft._2048.dtos.SelectGameDto;
 import com.szaszisoft._2048.models.Board;
 import com.szaszisoft._2048.models.BoardType;
 import com.szaszisoft._2048.models.Game;
 import com.szaszisoft._2048.services.BoardService;
 import com.szaszisoft._2048.services.GameService;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -24,16 +30,50 @@ public class WebController {
   }
 
   @GetMapping("/")
-  public String getBoard(Model model) {
-    Game game = gameService.getGameById(1L);
-    Board board = selectBoard(game, BoardType.PLAY);
-    model.addAttribute("board", board);
+  public String getMain(Model model, Game newGame) {
+    model.addAttribute("games", gameService.findAll());
+    model.addAttribute(newGame);
     return "index";
   }
 
+  @PostMapping("/game")
+  public String postGame(@ModelAttribute SelectGameDto selectGameDTO,
+                         HttpServletResponse httpServletResponse) {
+    System.out.println(selectGameDTO.getGameId());
+    Long gameId = selectGameDTO.getGameId();
+    if (gameId == 0 || gameId == null) {
+      gameId = gameService.startNewGame(selectGameDTO.getSize()).getId();
+    }
+    Cookie cookie = new Cookie("gameid", "" + gameId);
+    cookie.setHttpOnly(true);
+    cookie.setPath("/");
+    httpServletResponse.addCookie(cookie);
+    return "redirect:/game";
+  }
+
+  @GetMapping("/game")
+  public String getBoard(Model model,
+                         @CookieValue(value = "gameid", defaultValue = "0") Long gameId) {
+    if (gameId == 0) {
+      return "redirect:/";
+    }
+    Game game = gameService.getGameById(gameId);
+    if (game == null) {
+      return "redirect:/";
+    }
+    Board board = selectBoard(game, BoardType.PLAY);
+    model.addAttribute("board", board);
+    return "game";
+  }
+
   @GetMapping("/move/{id}")
-  public String getMoveBoard(@PathVariable String id, RedirectAttributes redirectAttributes) {
-    Game game = gameService.getGameById(1L);
+  public String getMoveBoard(@PathVariable String id,
+                             @CookieValue(value = "gameid", defaultValue = "0") Long gameId,
+                             RedirectAttributes redirectAttributes) {
+    if (gameId == 0) {
+      return "redirect:/";
+    }
+    Game game = gameService.getGameById(gameId);
     Board board = selectBoard(game, BoardType.PLAY);
     Board previous = selectBoard(game, BoardType.UNDO);
 
@@ -63,28 +103,34 @@ public class WebController {
     }
     boardService.save(board);
     boardService.save(previous);
-    return "redirect:/";
+    return "redirect:/game";
   }
 
   @GetMapping("/reset")
-  public String getReset() {
-    Game game = gameService.getGameById(1L);
+  public String getReset(@CookieValue(value = "gameid", defaultValue = "0") Long gameId) {
+    if (gameId == 0) {
+      return "redirect:/";
+    }
+    Game game = gameService.getGameById(gameId);
     Board board = selectBoard(game, BoardType.PLAY);
     Board previous = selectBoard(game, BoardType.UNDO);
     previous.populate(board);
     boardService.save(previous);
     board = boardService.resetBoard(board);
-    return "redirect:/";
+    return "redirect:/game";
   }
 
   @GetMapping("/undo")
-  public String getUndo() {
-    Game game = gameService.getGameById(1L);
+  public String getUndo(@CookieValue(value = "gameid", defaultValue = "0") Long gameId) {
+    if (gameId == 0) {
+      return "redirect:/";
+    }
+    Game game = gameService.getGameById(gameId);
     Board board = selectBoard(game, BoardType.PLAY);
     Board previous = selectBoard(game, BoardType.UNDO);
     board.populate(previous);
     boardService.save(board);
-    return "redirect:/";
+    return "redirect:/game";
   }
 
   private Board selectBoard(Game game, BoardType boardType) {
