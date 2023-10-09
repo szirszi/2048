f3e00a79505d3d0c5960eec748427ce1292f150d
Szabolcs Milan <szirszi@gmail.com>
2023-10-09 22:07:19 +0200

    Multiboard set up

--- src/main/java/com/szaszisoft/_2048/repositories/GameRepository.java
+++ src/main/java/com/szaszisoft/_2048/repositories/GameRepository.java
@@ -1,8 +1,10 @@
 package com.szaszisoft._2048.repositories;
 
 import com.szaszisoft._2048.models.Game;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
   Game getGameById(Long id);
+  List<Game> findAll();
 }
